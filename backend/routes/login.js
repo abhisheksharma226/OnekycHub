dc759d252e8639b8e19601081609978aebc29922
abhisheksharma226 <avisheksharma2004@gmail.com>
2025-03-26 23:03:31 +0530

    token expire time increase

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -47,7 +47,7 @@ console.log("Active tab:", loginType);
     const token = jwt.sign(
       { email: account.email, id: account._id }, // Payload
         process.env.JWT_SECRET, // Secret key
-      { expiresIn: "30s" } // Token expiry
+      { expiresIn: "1hr" } // Token expiry
     );
     console.log("Generated Token:", token); // Debug generated token
 
